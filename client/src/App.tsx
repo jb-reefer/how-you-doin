b87d8e57c463a38aef2c4ed66ee29c1b657ff96f
jb-reefer <james.reefer@gmail.com>
2019-03-23 18:35:26 -0400

    make tslint happy

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,64 +1,62 @@
-import React, { Component } from 'react';
-import { Alert } from './components/Alert';
-import { XYPlot, XAxis, YAxis, LineSeries, HorizontalGridLines, VerticalGridLines, Crosshair } from 'react-vis';
-import './App.scss';
+import "./App.scss";
+import { Alert } from "./components/Alert";
+import { Messages } from "./components/Messages";
+import { XYPlot, XAxis, YAxis, LineSeries, HorizontalGridLines, VerticalGridLines, Crosshair } from "react-vis";
+import React, { Component } from "react";
 
-interface CPUData {
+interface ICPUData {
   x: Date;
   y: number;
 }
 
-// TODO: better naming!
-interface AppState {
-  cpuData: CPUData[];
+interface IAppState {
+  cpuData: ICPUData[];
   crosshairValues: any;
 }
 
-class App extends Component<any, AppState> {
+class App extends Component<any, IAppState> {
   constructor(props) {
     super(props);
     this.state = {
       cpuData: [],
-      crosshairValues: []
+      crosshairValues: [],
     };
-    
+
     this.getCPUData().then(() => setInterval(this.getCPUData, 10 * 1000));
   }
 
-  getCPUData = () => {
-    return fetch('/api/cpu')
-      .then(response => { 
-        console.log(response);
-        return response.text()
-      })
-      .then(percent => {
-        const buffer: CPUData[] = this.state.cpuData.concat({
+  private getCPUData = () => {
+    return fetch("/api/cpu")
+      .then((response) => response.text())
+      .then((percent: string) => {
+        const buffer: ICPUData[] = this.state.cpuData.concat({
           x: new Date(),
-          y: parseFloat(percent)
+          y: parseFloat(percent),
         })
         this.setState({
-          cpuData: buffer
+          cpuData: buffer,
         });
       })
   }
 
   // TODO: pretty this up
-  _onMouseLeave = () => {
+  private _onMouseLeave = () => {
     this.setState({ crosshairValues: [] });
   };
 
-  _onNearestX = (value, { index }) => {
+  private _onNearestX = (value, { index }) => {
     this.setState({ crosshairValues: this.state.cpuData.map(d => d[index]) });
   };
 
   render = () => {
     return (
       <div className="App">
-        <Alert kind='Info'>Grumpy</Alert>
+        <div className="Graph">
+        {/* <Alert kind='Info'>Grumpy</Alert> */}
         <h1>CPU %</h1>
         <XYPlot 
-          height={400} 
-          width={900} 
+          height={400}
+          width={800}
           onMouseLeave={this._onMouseLeave}
           onNearestX={this._onNearestX}
         >
@@ -75,17 +73,19 @@ class App extends Component<any, AppState> {
             data={this.state.cpuData}
             opacity={1}
             strokeStyle="solid"
-            curve='curveStep'
+            curve="curveStep"
           >
           </LineSeries>
           <Crosshair values={this.state.crosshairValues}>
-            <div style={{ background: 'black' }}>
+            <div style={{ background: "black" }}>
               <h3>Values of crosshair:</h3>
               <p>x: {this.state.crosshairValues}</p>
               <p>y: {this.state.crosshairValues}</p>
             </div>
           </Crosshair>
         </XYPlot>
+        </div>
+        <Messages data={this.state.cpuData} />
       </div>
     );
   }
