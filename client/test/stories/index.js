4e3630d48df98f2d3fce77436e8c94840a80f05d
jb-reefer <james.reefer@gmail.com>
2019-03-29 14:01:13 -0400

    Created storybook for <CPUGraph />

--- client/test/stories/index.js
+++ client/test/stories/index.js
@@ -1,8 +1,10 @@
+import React from "react";
 import { storiesOf } from "@storybook/react";
+import { action } from '@storybook/addon-actions';
 import { Alert } from '../../src/components/Alert'
 import { Messages } from '../../src/components/Messages'
-import React from "react";
-import { action } from '@storybook/addon-actions';
+import { CPUGraph } from '../../src/components/CPUGraph'
+import dummyCPUData from "../../src/test/dummyData";
 
 storiesOf("Alert", module)
 .add("Alerts - All", () => (<>
@@ -31,3 +33,19 @@ storiesOf("Messages", module)
     {["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "Short", "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,", "Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"]}
   </Messages>
 ))
+
+storiesOf("CPUGraph", module)
+  .add("Single graph", () => (
+    <CPUGraph cpuData={dummyCPUData} />
+  ))
+  .add("4 graphs", () => (
+  <>
+    <CPUGraph cpuData = {dummyCPUData} />
+    <br />
+    <CPUGraph cpuData = {dummyCPUData} />
+    < br />
+    <CPUGraph cpuData = {dummyCPUData} />
+    < br />
+    <CPUGraph cpuData = {dummyCPUData} />
+  </>)
+);
