f1bd2489313177ae2e0b9e3f33e61c2d1673c2d4
jb-reefer <james.reefer@gmail.com>
2019-04-09 17:15:52 -0400

    Moved logic out of state on frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,96 +9,31 @@ export interface ICPUData {
   y: number;
 }
 
-interface IAppState {
+interface IAppProps {
   cpuData: { [s: string]: ICPUData[]; };
   messages: string[];
-  error?: Error;
+  alert?: string;
+  closeAlertHandler: () => void;
 }
 
-const SAMPLING_INTERVAL = 10 * 1000;
-
-class App extends Component<any, IAppState> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cpuData: { "all" : [] },
-      messages: [],
-    };
-  }
-
-  componentDidMount = () => {
-    this.getCPUData().then(() => setInterval(this.getCPUData, SAMPLING_INTERVAL));
-  }
-
-  render = () => {
-    return (
-      <div className="App">
-        <div className="Graph">
-        {this.state.error &&
-          <Alert kind="Error" closeHandler={this.closeAlertHandler}>
-            {this.state.error.message}
-          </Alert>}
-        {
-          Object.entries(this.state.cpuData).sort().map(([name, cpuData]) => (
-            <>
-              <h1>CPU {name}</h1>
-              <CPUGraph cpuData={cpuData} />
-            </>
-          ))
-        }
+const App = (props: IAppProps) => (
+  <div className="App">
+    <div className="Graph">
+    {props.alert &&
+      <Alert kind="Error" closeHandler={props.closeAlertHandler}>
+        {props.alert}
+      </Alert>}
+    {
+      Object.entries(props.cpuData).sort().map(([name, cpuData]) => (
+        <div key={name}>
+          <h1>CPU {name}</h1>
+          <CPUGraph cpuData={cpuData} />
         </div>
-        <Messages>{this.state.messages}</Messages>
-      </div>
-    );
-  }
-
-  closeAlertHandler = () => {
-    this.setState({
-      error: undefined,
-    });
-  }
-
-  tenMinutesOfSamples = () => {
-    const tenMinutesMills = 10 * 60 * 1000;
-    return tenMinutesMills / SAMPLING_INTERVAL;
-  }
-
-  // TODO: jest test this
-  addCPUDataToState = (cpuName: string, idle: number) => {
-    const cpuData = this.state.cpuData;
-    // TODO: there's a prettier way to do this
-    if (!cpuData[cpuName]) {
-      cpuData[cpuName] = [];
+      ))
     }
-    cpuData[cpuName] = cpuData[cpuName].concat({
-      x: new Date(),
-      y: 100.00 - idle,
-    });
-
-    // Only keep 10 minutes of samples
-    if (cpuData[cpuName].length + 1 > this.tenMinutesOfSamples()) {
-      cpuData[cpuName].shift();
-    }
-    this.setState({
-      cpuData,
-    });
-  }
-
-  getCPUData = () => {
-    return fetch("/api/cpu")
-      .then((response) => response.json())
-      .then((data: Array<{cpu: string, idle: number}>) => {
-        data.forEach((record) => this.addCPUDataToState(record.cpu, record.idle));
-      })
-      .catch((error: Error) => {
-        this.setState({
-          error,
-          messages: this.state.messages.concat(this.buildMessage(error.message)),
-        });
-      });
-  }
-
-  buildMessage = (text: string) => (new Date().toLocaleTimeString() + " : " + text);
-}
+    </div>
+    <Messages>{props.messages}</Messages>
+  </div>
+);
 
 export default App;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,59 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App, { ICPUData } from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Whenever the load for the past 2 minutes exceeds 1 on average, add a message saying that “High load generated an alert - load = { value }, triggered at { time } ”
+// Whenever the load average drops again below 1 on average for the past 2 minutes, Add another message explaining when the alert recovered.
+// Make sure all messages showing when alerting thresholds are crossed remain visible on the page for historical reasons.
+
+const SAMPLING_INTERVAL = 10 * 1000;
+const cpuData: { [s: string]: ICPUData[]; } = { all: [] };
+const messages: string[] = [];
+let message: string | undefined;
+
+// TODO: jest test this
+const StoreCPUData = (cpuName: string, idle: number) => {
+  cpuData[cpuName] = (cpuData[cpuName] || []).concat({
+    x: new Date(),
+    y: 100.00 - idle,
+  });
+
+  // Only keep 10 minutes of samples
+  if (cpuData[cpuName].length + 1 > tenMinutesOfSamples()) {
+    cpuData[cpuName].shift();
+  }
+}
+
+const getCPUData = () => {
+  return fetch("/api/cpu")
+    .then((response) => response.json())
+    .then((data: Array<{ cpu: string, idle: number }>) => {
+      data.forEach((record) => StoreCPUData(record.cpu, record.idle));
+    })
+    .catch((error: Error) => {
+      message = error.message;
+      messages.unshift(buildMessage(error.message));
+    });
+}
+
+const tenMinutesOfSamples = () => {
+  const tenMinutesMills = 10 * 60 * 1000;
+  return tenMinutesMills / SAMPLING_INTERVAL;
+};
+
+getCPUData().then(() => setInterval(getCPUData, SAMPLING_INTERVAL));
+
+const closeAlertHandler = () => {
+  message = undefined;
+};
+
+const buildMessage = (text: string) => (new Date().toLocaleTimeString() + " : " + text);
+
+ReactDOM.render(
+  <App
+    cpuData={cpuData}
+    messages={messages}
+    alert={message}
+    closeAlertHandler={closeAlertHandler}
+  />,
+  document.getElementById("root"));
